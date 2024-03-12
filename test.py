188c6f588d1a08998df6ce27a5aee497fa0b8921
https://gitlab.com/angel.cabezas <angel.cabezas@xertica.com>
2024-03-12 04:53:37 -0500

    File name variable fixed

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ def createFolder(folderPath, folderName):
     from google.colab import drive
     import os
     drive.mount('/content/gdrive')
-    os.mkdir(folderPath + '/' + rawDataFolderName)
+    os.mkdir(folderPath + '/' + folderName)
     print("Folder creado correctamente")
