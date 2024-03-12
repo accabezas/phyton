08a8c4f4344dfedf90fcbe4df41c54afe6908a5c
https://gitlab.com/angel.cabezas <angel.cabezas@xertica.com>
2024-03-12 04:51:12 -0500

    Import os

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 def createFolder(folderPath, folderName):
     from google.colab import drive
+    import os
     drive.mount('/content/gdrive')
     os.mkdir(folderPath + '/' + rawDataFolderName)
     print("Folder creado correctamente")
