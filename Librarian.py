6851b84324db12835bb8729ef7e3636daf34303d
https://gitlab.com/angel.cabezas <angel.cabezas@xertica.com>
2024-03-14 14:50:21 -0500

    Librarian class

---
+++ Librarian.py
@@ -0,0 +1,29 @@
+class Librarian:
+  def __init__(self, projectPath, projectName):
+    self.projectPath = projectPath
+    self.projectName = projectName
+
+  def createProjectFolders(self):
+    #* Google drive mount
+    from google.colab import drive
+    drive.mount('/content/gdrive')
+    #* Folders Creation
+    import os
+    os.mkdir(self.projectPath + '/' + self.projectName)
+    os.mkdir(self.projectPath + '/' + self.projectName + '/Raw')
+    os.mkdir(self.projectPath + '/' + self.projectName + '/Output')
+
+  def getRawFilePaths(self):
+    import os
+    files = os.listdir(self.projectPath + '/' + self.projectName + '/Raw')
+    return files
+
+  def getRawFolderPath(self):
+    rawFolderPath = self.projectPath + '/' + self.projectName + '/Raw'
+    return rawFolderPath
+
+  def saveOutput(self, fileName):
+    import shutil
+    outputFolder = self.projectPath + '/' + self.projectName + '/Output'
+    print(fileName)
+    shutil.copy("/content/" + fileName, outputFolder + "/" + fileName)
